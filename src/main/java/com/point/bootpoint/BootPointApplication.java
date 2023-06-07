63373dabd1084a1daab1bcc260e3afbad2f37315
dhkim <epyruskdh@gmail.com>
2023-06-07 23:11:21 +0900

    initial commit

---
+++ src/main/java/com/point/bootpoint/BootPointApplication.java
@@ -0,0 +1,13 @@
+package com.point.bootpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/point/bootpoint/BootPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.point.bootpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
